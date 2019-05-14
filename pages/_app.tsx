1462a73aa5639ae3c203863d9dadf3995428ff3a
Matt Moriarity <matt@mattmoriarity.com>
2019-05-14 15:24:17 -0600

    Fix large icons on SSR

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,14 @@ import React from "react"
 import App, { Container } from "next/app"
 import Router from "next/router"
 import { renewSession, isAuthenticated } from "../utils/auth0"
+import { config } from "@fortawesome/fontawesome-svg-core"
+
+config.autoAddCss = false
 
 import "normalizecss/normalize.css"
 import "typeface-rubik"
 import "typeface-ibm-plex-sans"
+import "@fortawesome/fontawesome-svg-core/styles.css"
 
 class MyApp extends App {
   state = {
