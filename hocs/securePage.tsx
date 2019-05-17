6144d30672c4594497bcdc25fcfdf94aa977e445
Matt Moriarity <matt@mattmoriarity.com>
2019-05-16 21:53:18 -0600

    Style login prompt for secure pages

--- hocs/securePage.tsx
+++ hocs/securePage.tsx
@@ -2,6 +2,14 @@ import React from "react"
 import withDefaultPage from "./defaultPage"
 import Link from "next/link"
 import Loading from "../components/loading"
+import { PageHeader } from "../components/header"
+import Head from "../components/head"
+import { spacing, colors, shadow, font } from "../utils/theme"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  faSignInAlt,
+  faExclamationTriangle,
+} from "@fortawesome/free-solid-svg-icons"
 
 interface Props {
   isAuthenticated: boolean
@@ -26,12 +34,64 @@ const securePage = (Page: any) =>
           return <Loading />
         } else {
           return (
-            <p>
-              You are not authorized to see this page.
+            <div className="container">
+              <Head title="Log In to Courier" />
+              <PageHeader>
+                <FontAwesomeIcon
+                  icon={faExclamationTriangle}
+                  className="header-icon"
+                />
+                Uh Oh!
+              </PageHeader>
+              <p>You need to be logged in to see this page.</p>
               <Link href="/login">
-                <a>Login</a>
+                <a>
+                  <FontAwesomeIcon icon={faSignInAlt} />
+                  Login
+                </a>
               </Link>
-            </p>
+              <style jsx>{`
+                .container {
+                  margin: 20vh auto;
+                  max-width: 600px;
+                  background-color: white;
+                  padding: 1rem 1rem 3rem 1rem;
+                  box-shadow: ${shadow.lg};
+                  border-top: 4px solid ${colors.primary[500]};
+                  display: flex;
+                  flex-direction: column;
+                  align-items: center;
+                }
+                .container > p {
+                  text-align: center;
+                  font-size: 1.2rem;
+                  margin-top: 0;
+                }
+                a {
+                  display: block;
+                  padding: ${spacing(3)};
+                  background-color: ${colors.primary[600]};
+                  color: white;
+                  text-decoration: none;
+                  border-radius: 0.5rem;
+                  box-shadow: ${shadow.md};
+                  font-size: 1.2rem;
+                  font-weight: 500;
+                  text-transform: uppercase;
+                  font-family: ${font.display};
+                }
+                a:hover {
+                  background-color: ${colors.primary[500]};
+                }
+                .container :global(svg) {
+                  margin-right: ${spacing(2)};
+                }
+                .container :global(.header-icon) {
+                  color: ${colors.primary[700]};
+                  font-size: 2.5rem;
+                }
+              `}</style>
+            </div>
           )
         }
       }
