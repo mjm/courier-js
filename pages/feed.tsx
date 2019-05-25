515e753dd84b815a179ba5d58a1e55fa0b9b23f3
Matt Moriarity <matt@mattmoriarity.com>
2019-05-24 19:44:37 -0700

    Improve appearance of feed details

--- pages/feed.tsx
+++ pages/feed.tsx
@@ -9,6 +9,7 @@ import { GetFeedDetailsComponent } from "../lib/generated/graphql-components"
 import Loading from "../components/loading"
 import { ErrorBox } from "../components/error"
 import Moment from "react-moment"
+import { shadow, spacing, colors } from "../utils/theme"
 
 interface Props {
   id: string
@@ -37,13 +38,31 @@ const Feed = ({ id }: Props) => {
                 <Head title={`${feed.feed.title} - Details`} />
 
                 <PageHeader>{feed.feed.title}</PageHeader>
-                <div>
-                  <p>{feed.feed.url}</p>
-                  <p>{feed.feed.homePageURL}</p>
-                  <p>
-                    <Moment>{feed.feed.refreshedAt}</Moment>
-                  </p>
-                  <p>Autopost? {feed.autopost ? "Yes" : "No"}</p>
+                <div className="form">
+                  <div className="field">
+                    <div className="label">Feed URL</div>
+                    <div className="entry">
+                      <a href={feed.feed.url}>{feed.feed.url}</a>
+                    </div>
+                  </div>
+                  <div className="field">
+                    <div className="label">Home Page</div>
+                    <div className="entry">
+                      <a href={feed.feed.homePageURL}>
+                        {feed.feed.homePageURL}
+                      </a>
+                    </div>
+                  </div>
+                  <div className="field">
+                    <div className="label">Last Checked</div>
+                    <div className="entry">
+                      <Moment fromNow>{feed.feed.refreshedAt}</Moment>
+                    </div>
+                  </div>
+                  <div className="field">
+                    <div className="label">Autopost</div>
+                    <div className="entry">{feed.autopost ? "Yes" : "No"}</div>
+                  </div>
                 </div>
               </>
             )
@@ -52,6 +71,33 @@ const Feed = ({ id }: Props) => {
           }
         }}
       </GetFeedDetailsComponent>
+      <style jsx>{`
+        .form {
+          margin-top: ${spacing(6)};
+          background-color: white;
+          box-shadow: ${shadow.md};
+          border-radius: 0.25rem;
+          padding: ${spacing(4)} ${spacing(4)};
+          display: flex;
+          flex-direction: column;
+        }
+        .field {
+          display: flex;
+          padding: ${spacing(1)} 0;
+        }
+        .label {
+          width: 180px;
+          color: ${colors.primary[800]};
+          font-weight: 500;
+        }
+        .entry {
+          color: ${colors.gray[900]};
+        }
+        a {
+          color: ${colors.primary[700]};
+          text-decoration: none;
+        }
+      `}</style>
     </Container>
   )
 }
