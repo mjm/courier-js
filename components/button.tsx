99467b320df3d3fc830a984887281ecdfca3b617
Matt Moriarity <matt@mattmoriarity.com>
2019-06-19 20:11:47 -0600

    Refactor button styles

--- components/button.tsx
+++ components/button.tsx
@@ -1,79 +1,88 @@
 import React, { forwardRef } from "react"
 import styled from "@emotion/styled"
-import { css } from "@emotion/core"
 import { space, SpaceProps, alignSelf, AlignSelfProps } from "styled-system"
-import { colors, spacing, shadow } from "../utils/theme"
 import { IconDefinition } from "@fortawesome/fontawesome-svg-core"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faSpinner } from "@fortawesome/free-solid-svg-icons"
+import Icon from "./icon"
 
-// get a styled component to refer to in StyledButton
-const ButtonIcon = styled(FontAwesomeIcon)``
+type Size = "small" | "medium" | "large"
 
 type StyledButtonProps = React.PropsWithoutRef<
   JSX.IntrinsicElements["button"]
 > & {
-  size: "small" | "medium" | "large"
+  size: Size
   color: "primary" | "red" | "blue"
   invert: boolean
 } & SpaceProps &
   AlignSelfProps
-const StyledButton = styled.button<StyledButtonProps>`
-  ${space}
-  ${alignSelf}
-  flex-shrink: 0;
-  border: 0;
-  line-height: 1.5em;
-  font-weight: 500;
-  border-radius: 0.6rem;
-  box-shadow: ${shadow.sm};
-  outline: none;
 
-  :disabled {
-    background-color: ${colors.gray[200]} !important;
-    color: ${colors.gray[500]} !important;
-    cursor: not-allowed;
-  }
+const colorStyles = ({
+  color,
+  invert,
+  theme,
+}: Pick<StyledButtonProps, "color" | "invert"> & { theme: any }) =>
+  invert
+    ? {
+        color: theme.colors[color][600],
+        backgroundColor: theme.colors[color][100],
+        ":hover": {
+          color: theme.colors[color][700],
+          backgroundColor: theme.colors[color][200],
+        },
+      }
+    : {
+        color: "white",
+        backgroundColor: theme.colors[color][600],
+        ":hover": {
+          backgroundColor: theme.colors[color][500],
+        },
+      }
 
-  ${ButtonIcon} {
-    margin-right: ${spacing(2)};
-  }
+const sizes: { [key in Size]: any } = {
+  small: ({ theme }: any) => ({
+    fontSize: theme.fontSizes[1],
+  }),
 
-  ${({ size }) => sizeStyles[size]}
-  ${({ color, invert }) => `
-    color: ${invert ? colors[color][600] : "white"};
-    background-color: ${invert ? colors[color][100] : colors[color][600]};
+  medium: ({ theme }: any) => ({
+    fontSize: theme.fontSizes[3],
+    boxShadow: theme.shadow.md,
+  }),
 
-    :hover {
-      color: ${invert ? colors[color][700] : "white"};
-      background-color: ${invert ? colors[color][200] : colors[color][500]};
-    }
-  `}
-`
+  large: ({ theme }: any) => ({
+    padding: `${theme.space[2]} ${theme.space[3]}`,
+    fontSize: theme.fontSizes[5],
+    boxShadow: theme.shadow.md,
+  }),
+}
+
+const sizeStyles = (props: { size: Size; theme: any }) =>
+  sizes[props.size](props)
+
+const StyledButton = styled.button<StyledButtonProps>(
+  space,
+  alignSelf,
+  ({ theme }: any) => ({
+    flexShrink: 0,
+    border: 0,
+    lineHeight: theme.lineHeights.normal,
+    fontWeight: 500,
+    borderRadius: "0.5rem",
+    boxShadow: theme.shadows.sm,
+    outline: "none",
+    ":disabled": {
+      backgroundColor: `${theme.colors.gray[200]} !important`,
+      color: `${theme.colors.gray[500]} !important`,
+      cursor: "not-allowed",
+    },
+  }),
+  colorStyles,
+  sizeStyles
+)
 
 StyledButton.defaultProps = {
   px: 2,
   py: 1,
   alignSelf: "flex-start",
-}
-
-const sizeStyles = {
-  small: css`
-    font-size: 0.9rem;
-
-    ${ButtonIcon} {
-      margin-right: ${spacing(1)};
-    }
-  `,
-  medium: css`
-    font-size: 1.2rem;
-    box-shadow: ${shadow.md};
-  `,
-  large: css`
-    padding: ${spacing(2)} ${spacing(4)};
-    font-size: 1.4rem;
-    box-shadow: ${shadow.md};
-  `,
 }
 
 type Props = React.PropsWithoutRef<JSX.IntrinsicElements["button"]> & {
@@ -111,7 +120,8 @@ export const Button = forwardRef<HTMLButtonElement, Props>(
         {...props}
       >
         {icon && (
-          <ButtonIcon
+          <Icon
+            mr={size === "small" ? 1 : 2}
             icon={spin && !useSameIconWhileSpinning ? faSpinner : icon}
             spin={spin}
           />
