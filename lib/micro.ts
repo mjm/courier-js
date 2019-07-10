0cb53ac1c0f892b68e71ae9e295e318bf1a0936a
Matt Moriarity <matt@mattmoriarity.com>
2019-07-09 22:07:21 -0600

    Delete micro wrapper

--- lib/micro.ts
+++
@@ -1,9 +0,0 @@
-import { run, RequestHandler } from "micro"
-
-export * from "micro"
-
-export default function(fn: RequestHandler): RequestHandler {
-  return (req, res) => {
-    return run(req, res, fn)
-  }
-}
