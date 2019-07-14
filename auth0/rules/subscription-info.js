4e75f8cad44a2f14aaab8ac2055faf2d917ebb7e
Matt Moriarity <matt@mattmoriarity.com>
2019-07-14 08:39:58 -0600

    Fix stripe auth rule when no metadata is present

--- auth0/rules/subscription-info.js
+++ auth0/rules/subscription-info.js
@@ -1,12 +1,12 @@
 function (user, context, callback) {
   const namespace = "https://courier.blog/"
-  if (user.app_metadata.stripe_customer_id) {
-    context.accessToken[namespace + "customer_id"] =
-      user.app_metadata.stripe_customer_id
+  const metadata = user.app_metadata || {}
+  if (metadata.stripe_customer_id) {
+    context.accessToken[namespace + "customer_id"] = metadata.stripe_customer_id
   }
-  if (user.app_metadata.stripe_subscription_id) {
+  if (metadata.stripe_subscription_id) {
     context.accessToken[namespace + "subscription_id"] =
-      user.app_metadata.stripe_subscription_id
+      metadata.stripe_subscription_id
   }
   callback(null, user, context)
 }
