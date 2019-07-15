6f6f7836c5ed3567213fe10a282b4d55b2fd6d27
Matt Moriarity <matt@mattmoriarity.com>
2019-07-14 21:39:47 -0600

    Allow reactivating a canceled subscription

--- lib/services/billing_service.ts
+++ lib/services/billing_service.ts
@@ -23,26 +23,35 @@ class BillingService {
     email?: string | null,
     tokenId?: string | null
   ): Promise<any> {
-    let customerId: string
-    if (email && tokenId) {
-      console.log("Creating subscription for", email, "with token", tokenId)
-      const customer = await this.stripe.customers.create({
-        email,
-        source: tokenId,
-      })
-      this.customerLoader.prime(customer.id, customer)
-      customerId = customer.id
+    const createNewCustomer = email && tokenId
+    const existingSubscription = await this.getSubscription()
+    if (existingSubscription) {
+      // TODO handle create new customer with existing subscription
+      if (existingSubscription.status === "active") {
+        if (existingSubscription.cancel_at_period_end) {
+          console.log(
+            "Reactivating user's canceled but unexpired subscription."
+          )
+          const subscription = await this.stripe.subscriptions.update(
+            existingSubscription.id,
+            { cancel_at_period_end: false }
+          )
+          this.subscriptionLoader.replace(subscription.id, subscription)
+          return
+        }
 
-      await this.userService.update({ stripe_customer_id: customerId })
-    } else {
-      const userInfo = await this.userService.getUserInfo()
-      if (!userInfo.stripe_customer_id) {
-        throw new Error("There is no existing payment method to subscribe with")
+        throw new Error(
+          "Cannot subscribe: you already have an active subscription"
+        )
       }
-      customerId = userInfo.stripe_customer_id
 
-      console.log("Creating subscription for existing customer", customerId)
+      console.log("User has existing non-active subscription, deleting it.")
+      await this.stripe.subscriptions.del(existingSubscription.id)
     }
+
+    const customerId = createNewCustomer
+      ? await this.createCustomer(email!, tokenId!)
+      : await this.requireExistingCustomer()
 
     const subscription = await this.stripe.subscriptions.create({
       customer: customerId,
@@ -67,6 +76,43 @@ class BillingService {
       { cancel_at_period_end: true }
     )
     this.subscriptionLoader.replace(subscription.id, subscription)
+  }
+
+  private async createCustomer(
+    email: string,
+    tokenId: string
+  ): Promise<string> {
+    console.log("Creating new subscription with token", tokenId)
+    const customer = await this.stripe.customers.create({
+      email,
+      source: tokenId,
+    })
+    this.customerLoader.prime(customer.id, customer)
+
+    await this.userService.update({ stripe_customer_id: customer.id })
+    return customer.id
+  }
+
+  private async requireExistingCustomer(): Promise<string> {
+    const userInfo = await this.userService.getUserInfo()
+    if (!userInfo.stripe_customer_id) {
+      throw new Error("There is no existing payment method to subscribe with")
+    }
+
+    console.log(
+      "Creating subscription for existing customer",
+      userInfo.stripe_customer_id
+    )
+    return userInfo.stripe_customer_id
+  }
+
+  private async getSubscription(): Promise<Stripe.subscriptions.ISubscription | null> {
+    const userInfo = await this.userService.getUserInfo()
+    if (!userInfo.stripe_subscription_id) {
+      return null
+    }
+
+    return await this.subscriptionLoader.load(userInfo.stripe_subscription_id)
   }
 }
 
