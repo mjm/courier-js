2e5bcc4dd81f1a6668e05ce0e73eff7011ac2f16
Matt Moriarity <matt@mattmoriarity.com>
2019-07-14 22:17:06 -0600

    Fix timestamp parsing to assume UTC

--- lib/db.ts
+++ lib/db.ts
@@ -1,14 +1,13 @@
 import { createPool, InterceptorType } from "@mmoriarity/slonik"
 import { createInterceptors } from "slonik-interceptor-preset"
+import moment from "moment"
 
 const pool = createPool(process.env.DATABASE_URL || "", {
   typeParsers: [
-    // we don't use the XML type, but we need something to be here to not
-    // use slonik's default type parsers, which muck up our whole Date situation
     {
-      name: "xml",
+      name: "timestamp",
       parse(value) {
-        return value
+        return value && moment.utc(value).toDate()
       },
     },
   ],
