1e7d6ce9a2bf57d7b51b7b28ef15315573f3ae78
Matt Moriarity <matt@mattmoriarity.com>
2020-02-15 23:22:16 -0700

    Generate tweet previews

--- internal/read/tweets/tweet_queries.go
+++ internal/read/tweets/tweet_queries.go
@@ -14,6 +14,7 @@ import (
 	"github.com/mjm/courier-js/internal/pager"
 	"github.com/mjm/courier-js/internal/read/tweets/queries"
 	"github.com/mjm/courier-js/internal/shared/tweets"
+	"github.com/mjm/courier-js/pkg/htmltweets"
 )
 
 var (
@@ -27,6 +28,7 @@ type TweetQueries interface {
 	Get(context.Context, TweetID) (*Tweet, error)
 	// Paged fetches a paged and possibly filtered subset of a user's tweets.
 	Paged(context.Context, string, Filter, pager.Options) (*pager.Connection, error)
+	GeneratePreviews(context.Context, PreviewPost) ([]*PreviewTweet, error)
 }
 
 type tweetQueries struct {
@@ -80,6 +82,44 @@ func (q *tweetQueries) Get(ctx context.Context, id TweetID) (*Tweet, error) {
 
 func (q *tweetQueries) Paged(ctx context.Context, userID string, filter Filter, opts pager.Options) (*pager.Connection, error) {
 	return pager.Paged(ctx, q.db, &tweetPager{Filter: filter, UserID: userID}, opts)
+}
+
+type PreviewPost struct {
+	URL         string
+	Title       string
+	HTMLContent string
+}
+
+type PreviewTweet struct {
+	Action    TweetAction
+	Body      string
+	MediaURLs []string
+	RetweetID string
+}
+
+func (q *tweetQueries) GeneratePreviews(_ context.Context, p PreviewPost) ([]*PreviewTweet, error) {
+	translated, err := htmltweets.Translate(htmltweets.Input{
+		Title: p.Title,
+		URL:   p.URL,
+		HTML:  p.HTMLContent,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	// add the tweets in reverse order, so the last ones come first
+	var ts []*PreviewTweet
+	for i := len(translated) - 1; i >= 0; i-- {
+		t := translated[i]
+		ts = append(ts, &PreviewTweet{
+			Action:    TweetAction(t.Action),
+			Body:      t.Body,
+			MediaURLs: t.MediaURLs,
+			RetweetID: t.RetweetID,
+		})
+	}
+
+	return ts, nil
 }
 
 func (q *tweetQueries) HandleEvent(ctx context.Context, evt interface{}) {
--- internal/resolvers/preview.go
+++ internal/resolvers/preview.go
@@ -1,7 +1,12 @@
 package resolvers
 
 import (
+	"context"
+	"strings"
+
+	"github.com/mjm/courier-js/internal/pager"
 	"github.com/mjm/courier-js/internal/read/feeds"
+	"github.com/mjm/courier-js/internal/read/tweets"
 )
 
 type FeedPreview struct {
@@ -21,25 +26,48 @@ func (fp *FeedPreview) HomePageURL() string {
 	return fp.feed.HomePageURL
 }
 
-func (fp *FeedPreview) Tweets() []*TweetPreview {
-	return nil
+func (fp *FeedPreview) Tweets(ctx context.Context) ([]*TweetPreview, error) {
+	conn, err := fp.q.Posts.Paged(ctx, fp.feed.ID, pager.First(10, nil))
+	if err != nil {
+		return nil, err
+	}
+
+	var previews []*TweetPreview
+	for _, edge := range conn.Edges {
+		p := edge.(*feeds.Post)
+		ts, err := fp.q.Tweets.GeneratePreviews(ctx, tweets.PreviewPost{
+			URL:         p.URL,
+			Title:       p.Title,
+			HTMLContent: p.HTMLContent,
+		})
+		if err != nil {
+			return nil, err
+		}
+
+		for _, t := range ts {
+			previews = append(previews, &TweetPreview{tweet: t})
+		}
+	}
+
+	return previews, nil
 }
 
 type TweetPreview struct {
+	tweet *tweets.PreviewTweet
 }
 
 func (tp *TweetPreview) Action() string {
-	return ""
+	return strings.ToUpper(string(tp.tweet.Action))
 }
 
 func (tp *TweetPreview) Body() string {
-	return ""
+	return tp.tweet.Body
 }
 
 func (tp *TweetPreview) MediaURLs() []string {
-	return nil
+	return tp.tweet.MediaURLs
 }
 
 func (tp *TweetPreview) RetweetID() string {
-	return ""
+	return tp.tweet.RetweetID
 }
