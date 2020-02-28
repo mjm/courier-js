aeb913082404690455b5547270a380f09f486ec9
Matt Moriarity <matt@mattmoriarity.com>
2020-02-27 22:14:56 -0700

    Add cloud tasks API dependency

--- terraform/gcp/tasks.tf
+++ terraform/gcp/tasks.tf
@@ -1,4 +1,8 @@
 resource "google_cloud_tasks_queue" "tasks" {
   name     = "tasks-queue"
   location = "us-central1"
+
+  depends_on = [
+    google_project_service.services["cloudtasks.googleapis.com"]
+  ]
 }
