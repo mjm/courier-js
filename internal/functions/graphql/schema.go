c04e6312818dc5ad5807583dd94e4cee70d575db
Matt Moriarity <matt@mattmoriarity.com>
2020-03-11 21:24:36 -0600

    Use OpenTelemetry API for GraphQL

--- internal/functions/graphql/schema.go
+++ internal/functions/graphql/schema.go
@@ -4,11 +4,10 @@ import (
 	"github.com/mjm/graphql-go"
 
 	"github.com/mjm/courier-js/internal/resolvers"
-	"github.com/mjm/courier-js/internal/trace"
 )
 
 func NewSchema(schemaString string, resolver *resolvers.Root) (*graphql.Schema, error) {
 	return graphql.ParseSchema(schemaString, resolver,
-		graphql.Tracer(trace.GraphQLTracer{}),
+		graphql.Tracer(GraphQLTracer{}),
 		graphql.UseFieldResolvers())
 }
---
+++ internal/functions/graphql/trace.go
@@ -0,0 +1,66 @@
+package graphql
+
+import (
+	"context"
+
+	"github.com/mjm/graphql-go/errors"
+	"github.com/mjm/graphql-go/introspection"
+	gqltrace "github.com/mjm/graphql-go/trace"
+	"go.opentelemetry.io/otel/api/global"
+	"go.opentelemetry.io/otel/api/key"
+	"go.opentelemetry.io/otel/api/trace"
+)
+
+var tracer = global.TraceProvider().Tracer("courier.blog/internal/functions/graphql")
+
+var (
+	queryKey         = key.New("gql.query")
+	operationNameKey = key.New("gql.operation_name")
+	typeKey          = key.New("gql.type")
+	fieldKey         = key.New("gql.field")
+)
+
+// GraphQLTracer implements gqltrace.Tracer for tracing GraphQL requests
+type GraphQLTracer struct{}
+
+var _ gqltrace.Tracer = GraphQLTracer{}
+
+func (GraphQLTracer) TraceQuery(ctx context.Context, queryString string, operationName string, variables map[string]interface{}, varTypes map[string]*introspection.Type) (context.Context, gqltrace.TraceQueryFinishFunc) {
+	if operationName == "IntrospectionQuery" {
+		return ctx, func([]*errors.QueryError) {}
+	}
+
+	ctx, span := tracer.Start(ctx, "graphql.Query",
+		trace.WithAttributes(
+			queryKey.String(queryString),
+			operationNameKey.String(operationName)))
+
+	return ctx, func(errs []*errors.QueryError) {
+		for _, err := range errs {
+			span.RecordError(ctx, err)
+		}
+		span.End()
+	}
+}
+
+func (GraphQLTracer) TraceField(ctx context.Context, label, typeName, fieldName string, trivial bool, args map[string]interface{}) (context.Context, gqltrace.TraceFieldFinishFunc) {
+	if trivial {
+		return ctx, func(*errors.QueryError) {}
+	}
+
+	if fieldName == "__schema" || typeName == "__Schema" || typeName == "__Type" {
+		return ctx, func(*errors.QueryError) {}
+	}
+
+	ctx, span := tracer.Start(ctx, label,
+		trace.WithAttributes(
+			typeKey.String(typeName),
+			fieldKey.String(fieldName)))
+
+	return ctx, func(err *errors.QueryError) {
+		if err != nil {
+			span.RecordError(ctx, err)
+		}
+		span.End()
+	}
+}
