3082474695638ce0d93653a823e2026c24329871
Matt Moriarity <matt@mattmoriarity.com>
2020-03-14 22:52:46 -0600

    Clean up trace package

--- internal/trace/trace.go
+++ internal/trace/trace.go
@@ -1,40 +1,15 @@
 package trace
 
 import (
-	"context"
-	"fmt"
 	"os"
-	"time"
 
-	"github.com/honeycombio/libhoney-go"
 	"github.com/honeycombio/opentelemetry-exporter-go/honeycomb"
-	"go.opentelemetry.io/otel/api/core"
 	"go.opentelemetry.io/otel/api/global"
-	"go.opentelemetry.io/otel/api/key"
-	"go.opentelemetry.io/otel/api/trace"
 	exporttrace "go.opentelemetry.io/otel/sdk/export/trace"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 
 	"github.com/mjm/courier-js/internal/secret"
 )
-
-type contextKey struct{}
-
-var traceContextKey contextKey
-
-type traceContext struct {
-	traceID   string
-	spanID    string
-	parentID  string
-	name      string
-	timestamp time.Time
-	builder   *libhoney.Builder
-	fields    Fields
-	sent      bool
-}
-
-// Fields is a map of string keys for adding data to a trace event.
-type Fields map[string]interface{}
 
 type ServiceName string
 
@@ -64,83 +39,4 @@ func Init(cfg secret.GCPConfig, svcname string) error {
 	}
 	global.SetTraceProvider(tp)
 	return nil
-}
-
-var tracer = global.TraceProvider().Tracer("courier.blog/internal/trace")
-
-// Start beings a new trace or span.
-func Start(ctx context.Context, name string) context.Context {
-	ctx, _ = tracer.Start(ctx, name)
-	return ctx
-}
-
-// Finish finishes a span or tracing, sending the corresponding event.
-func Finish(ctx context.Context) {
-	span := trace.SpanFromContext(ctx)
-	span.End()
-}
-
-// AddField sets a single field on the current span.
-func AddField(ctx context.Context, k string, value interface{}) {
-	span := trace.SpanFromContext(ctx)
-	span.SetAttributes(key.String(k, fmt.Sprintf("%s", value)))
-}
-
-// Add sets a map of fields on the current span.
-func Add(ctx context.Context, fields Fields) {
-	span := trace.SpanFromContext(ctx)
-
-	var kvs []core.KeyValue
-	for k, v := range fields {
-		kvs = append(kvs, key.String(k, fmt.Sprintf("%s", v)))
-	}
-
-	span.SetAttributes(kvs...)
-}
-
-// Error sets the error on the current span.
-func Error(ctx context.Context, err error) {
-	span := trace.SpanFromContext(ctx)
-	span.RecordError(ctx, err)
-}
-
-func GetTraceID(ctx context.Context) string {
-	trace := getTraceContext(ctx)
-	if trace == nil {
-		return ""
-	}
-
-	return trace.traceID
-}
-
-func GetSpanID(ctx context.Context) string {
-	trace := getTraceContext(ctx)
-	if trace == nil {
-		return ""
-	}
-
-	return trace.spanID
-}
-
-func SetParent(ctx context.Context, traceID string, parentID string) {
-	if traceID == "" || parentID == "" {
-		return
-	}
-
-	trace := getTraceContext(ctx)
-	if trace == nil {
-		return
-	}
-
-	trace.traceID = traceID
-	trace.parentID = parentID
-}
-
-func getTraceContext(ctx context.Context) *traceContext {
-	v := ctx.Value(&traceContextKey)
-	if v == nil {
-		return nil
-	}
-
-	return v.(*traceContext)
 }
