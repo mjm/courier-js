5c65b28ff003e17f09afc4ae7d50d646c1380bd7
Matt Moriarity <matt@mattmoriarity.com>
2020-04-25 23:06:36 -0600

    Flush events after finishing requests

--- internal/functions/lambda.go
+++ internal/functions/lambda.go
@@ -6,8 +6,13 @@ import (
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/awslabs/aws-lambda-go-api-proxy/httpadapter"
+	"github.com/honeycombio/libhoney-go"
 )
 
 func WrapInLambda(h http.Handler) func(context.Context, events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	return httpadapter.New(h).ProxyWithContext
+	return func(ctx context.Context, r events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+		res, err := httpadapter.New(h).ProxyWithContext(ctx, r)
+		libhoney.Flush()
+		return res, err
+	}
 }
