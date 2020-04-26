dabcdb14ddac0f5ca9ece429c285d39704ad3241
Matt Moriarity <matt@mattmoriarity.com>
2020-04-26 11:26:15 -0600

    Add API method for ping endpoint

--- terraform/aws/api.tf
+++ terraform/aws/api.tf
@@ -43,6 +43,29 @@ resource "aws_api_gateway_integration" "pusherauth" {
   uri                     = aws_lambda_function.pusherauth.invoke_arn
 }
 
+resource "aws_api_gateway_resource" "ping" {
+  rest_api_id = aws_api_gateway_rest_api.courier.id
+  parent_id   = aws_api_gateway_rest_api.courier.root_resource_id
+  path_part   = "ping"
+}
+
+resource "aws_api_gateway_method" "ping" {
+  rest_api_id   = aws_api_gateway_rest_api.courier.id
+  resource_id   = aws_api_gateway_resource.ping.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "ping" {
+  rest_api_id = aws_api_gateway_rest_api.courier.id
+  resource_id = aws_api_gateway_method.ping.resource_id
+  http_method = aws_api_gateway_method.ping.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.ping.invoke_arn
+}
+
 resource "aws_api_gateway_deployment" "courier" {
   depends_on = [
     aws_api_gateway_integration.pusherauth,
@@ -58,6 +81,10 @@ output "base_url" {
 
 output "pusherauth_url" {
   value = "${aws_api_gateway_deployment.courier.invoke_url}${aws_api_gateway_resource.pusherauth.path}"
+}
+
+output "ping_url" {
+  value = "${aws_api_gateway_deployment.courier.invoke_url}${aws_api_gateway_resource.ping.path}"
 }
 
 resource "aws_ssm_parameter" "dynamo_table_name" {
