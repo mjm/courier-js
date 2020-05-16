6acb4df7cd9db647f826be1d11a4d3693db6772f
Matt Moriarity <matt@mattmoriarity.com>
2020-05-15 22:25:40 -0600

    Rearrange views so that opening settings doesn't re-run a query

--- ios/Courier/CourierNext/Tweets Screen/TweetsScreen.swift
+++ ios/Courier/CourierNext/Tweets Screen/TweetsScreen.swift
@@ -10,65 +10,61 @@ query TweetsScreenQuery($filter: TweetFilter!) {
 """)
 
 struct TweetsScreen: View {
-    @Environment(\.endpoint) var endpoint
-    @Environment(\.relayEnvironment) var environment
-    @Environment(\.authActions) var authActions
-
     @State private var selectedTag = 0
-    @State private var isSettingsPresented = false
 
     init() {
         print("creating new tweets screen")
     }
 
     var body: some View {
-        VStack {
-            NavigationView {
-                VStack(spacing: 0) {
-                    Picker(selection: $selectedTag, label: EmptyView()) {
-                        Text("Upcoming").tag(0)
-                        Text("Past").tag(1)
-                    }
-                        .pickerStyle(SegmentedPickerStyle())
-                        .padding()
-
-                    Divider()
-
-                    RelayQuery(
-                        op: TweetsScreenQuery(),
-                        variables: .init(filter: selectedTag == 0 ? .upcoming : .past),
-                        fetchPolicy: .storeAndNetwork,
-                        loadingContent: LoadingView(text: "Loading tweets…"),
-                        errorContent: { ErrorView(error: $0) },
-                        dataContent: { data in
-                            Group {
-                                if data?.viewer == nil {
-                                    Spacer()
-                                } else {
-                                    TweetsList(tweets: data!.viewer!)
-                                }
-                            }
-                        }
-                    ).frame(maxHeight: .infinity)
+        NavigationView {
+            VStack(spacing: 0) {
+                Picker(selection: $selectedTag, label: EmptyView()) {
+                    Text("Upcoming").tag(0)
+                    Text("Past").tag(1)
                 }
-                    .navigationBarTitle("Tweets", displayMode: .inline)
-                    .navigationBarItems(leading: Button(
-                        action: { self.isSettingsPresented = true },
-                        label: { Image(systemName: "gear") }))
+                    .pickerStyle(SegmentedPickerStyle())
+                    .padding()
 
-            }
-            Text("").hidden().sheet(isPresented: $isSettingsPresented) {
-                SettingsScreen(isPresented: self.$isSettingsPresented)
-                    .environment(\.endpoint, self.endpoint)
-                    .environment(\.relayEnvironment, self.environment)
-                    .environment(\.authActions, self.authActions)
-            }
+                Divider()
+
+                RelayQuery(
+                    op: TweetsScreenQuery(),
+                    variables: .init(filter: selectedTag == 0 ? .upcoming : .past),
+                    fetchPolicy: .storeAndNetwork,
+                    loadingContent: LoadingView(text: "Loading tweets…"),
+                    errorContent: { ErrorView(error: $0) },
+                    dataContent: { Inner(data: $0) }
+                ).frame(maxHeight: .infinity)
+            }.navigationBarTitle("Tweets", displayMode: .inline)
         }
     }
-}
 
-struct TweetsScreen_Previews: PreviewProvider {
-    static var previews: some View {
-        TweetsScreen()
+    struct Inner: View {
+        @Environment(\.endpoint) var endpoint
+        @Environment(\.relayEnvironment) var environment
+        @Environment(\.authActions) var authActions
+        @State private var isSettingsPresented = false
+
+        let data: TweetsScreenQuery.Data?
+
+        var body: some View {
+            Group {
+                if data?.viewer == nil {
+                    Spacer()
+                } else {
+                    TweetsList(tweets: data!.viewer!)
+                }
+            }
+                .navigationBarItems(leading: Button(
+                    action: { self.isSettingsPresented = true },
+                    label: { Image(systemName: "gear") }))
+                .sheet(isPresented: $isSettingsPresented) {
+                    SettingsScreen(isPresented: self.$isSettingsPresented)
+                        .environment(\.endpoint, self.endpoint)
+                        .environment(\.relayEnvironment, self.environment)
+                        .environment(\.authActions, self.authActions)
+                }
+        }
     }
 }
