c632471f868983a389328e3360907656fec199ac
Matt Moriarity <matt@mattmoriarity.com>
2020-05-20 22:14:27 -0600

    Fix Auth0 terraform I think

--- terraform/auth0/resource_servers.tf
+++ terraform/auth0/resource_servers.tf
@@ -1,5 +1,5 @@
 resource "auth0_resource_server" "api" {
-  name       = "Courier API"
+  name = "Courier API"
   identifier = var.api_identifier
 
   skip_consent_for_verifiable_first_party_clients = true
@@ -7,8 +7,9 @@ resource "auth0_resource_server" "api" {
   # This allows refresh tokens to be issued, which is especially important for the iOS app
   # to not have weird disruptions.
   allow_offline_access = true
+  token_dialect = "access_token"
 
-  token_lifetime         = 86400
+  token_lifetime = 86400
   token_lifetime_for_web = 7200
-  signing_alg            = "RS256"
+  signing_alg = "RS256"
 }
